83865287cf2e69ac20f089fb5d777a753269e0a2
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-11-27 10:59:52 +0000

    Edit Login page

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,5 +1,23 @@
 export default function Login() {
     return(
-        <div></div>
+        <div>
+            <h1>Login Page</h1>
+            <hr />
+            <table>
+                <tr>
+                    <td aligh="right">Username: </td>
+                    <td>
+                        <input type="text" name="username"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td aligh="right">Password: </td>
+                    <td>
+                        <input type="text" name="password"/>
+                    </td>
+                </tr>
+                <button>Login</button>
+            </table>
+        </div>
     )
 }
